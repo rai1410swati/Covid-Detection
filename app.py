1a79046f535a43c788f15a734baeb88627114741
Swati Rai <72435146+rai1410swati@users.noreply.github.com>
2021-06-12 01:32:04 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from keras.models import load_model
+from keras.preprocessing import image
+
+app = Flask(__name__)
+
+dic = {0 : 'Covid', 1 : 'Normal'}
+
+model = load_model('transfer_learning_model.h5')
+
+model.make_predict_function()
+
+def predict_label(img_path):
+	i = image.load_img(img_path, target_size=(224,224))
+	i = image.img_to_array(i)/255.0
+	i = i.reshape(1, 224,224,3)
+	p = model.predict_classes(i)
+	return dic[p[0]]
+
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("index.html")
+
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "images/" + img.filename	
+		img.save(img_path)
+
+		p = predict_label(img_path)
+
+	return render_template("index.html", prediction = p, img_path = img_path)
+
+
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
